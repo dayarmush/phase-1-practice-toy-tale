81d6ee003204c7215d1fdac2b368683af83d381e
dovid yarmush <yarmushda@gmail.com>
2023-05-09 13:45:27 -0400

    made function to render current toys to dom

--- src/index.js
+++ src/index.js
@@ -13,3 +13,35 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+fetch('http://localhost:3000/toys')
+.then(resp => resp.json())
+.then(data => data.forEach(renderToys))
+
+// renders the toys o the page
+function renderToys(toy) {
+  const cardDiv = document.createElement('div');
+    const h2 = document.createElement('h2');
+    const img = document.createElement('img');
+    const p = document.createElement('p');
+    const btn = document.createElement('button');
+    
+    cardDiv.setAttribute('class', 'card')
+    
+    h2.innerText = toy.name
+    cardDiv.appendChild(h2)
+
+    img.src = toy.image
+    img.setAttribute('class', 'toy-avatar')
+    cardDiv.appendChild(img)
+
+    p.textContent = `${toy.likes} likes`
+    cardDiv.appendChild(p)
+
+    btn.setAttribute('class', 'like-btn')
+    btn.textContent = 'Like ❤️'
+    cardDiv.appendChild(btn)
+
+    document.querySelector('#toy-collection').appendChild(cardDiv)
+}
+
